0f439423ec4347144bd671502afb0615459e6a2a
jared.k <jaredassociates@gmail.com>
2026-07-05 01:03:11 +0300

    creating the delete route and error message

--- app.py
+++ app.py
@@ -29,19 +29,45 @@ def find_event(event_id):
 def index():
     return jsonify({"message":"Welcome to the Events API"})
 
+@app.route("/events",methods=["GET"])
+def get_events():
+    return jsonify([event.to_dict() for event in events]), 200
+
 @app.route("/events", methods=["POST"])
 def create_event():
+    data = request.get_json()
     # TODO: Task 2 - Design and Develop the Code
 
     # TODO: Task 3 - Implement the Loop and Process Each Element
 
     # TODO: Task 4 - Return and Handle Results
     pass
+    if not data or "title" not in data:
+        return jsonify({"error": "Missing required field: title"}), 400
+    
+    new_id = max((event.id for event in events), default=0) + 1
+    new_event = Event(new_id, data["title"])
+    events.append(new_event)
+
+    return jsonify(new_event.to_dict()), 201
 
 # TODO: Task 1 - Define the Problem
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
+    event = find_event(event_id)
+     
+    if event is None:
+        return jsonify({"error":f"Event with id{event_id} not found"}), 404
+    
+    data =request.get_json()
+
+    if not data or "title" not in data:
+        return jsonify({"error":"Missing required field: title"}), 400
+    
+    event.title = data["title"]
+
+    return jsonify(event.to_dict()), 200
     # TODO: Task 2 - Design and Develop the Code
 
     # TODO: Task 3 - Implement the Loop and Process Each Element
